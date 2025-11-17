ce7e9d0025d3e7c1e018584d9acef28558693335
Najmun Nahar Neha <najmun.nahar.nehaa@email.com>
2025-11-18 00:47:24 +0600

    add: add Post

---
+++ src/stores/posts.js
@@ -0,0 +1,64 @@
+import { defineStore } from 'pinia'
+
+export const usePostsStore = defineStore('posts-store', {
+  // Data
+  state() {
+    return {
+      posts: [
+        {
+          id: 1,
+          title: 'This great book!',
+          body: 'tellus integer feugiat scelerisque varius morbi enim nunc faucibus a pellentesque sit',
+          author: 'Sarah Doe',
+          created_at: '11/06/2023',
+          is_saved: false,
+        },
+        {
+          id: 2,
+          title: 'Coding is fun!?',
+          body: 'tellus integer feugiat scelerisque varius morbi enim nunc faucibus a pellentesque sit',
+          author: 'Jon Doe',
+          created_at: '06/04/2023',
+          is_saved: false,
+        },
+        {
+          id: 3,
+          title: 'Vue js VS React',
+          body: 'tellus integer feugiat scelerisque varius morbi enim nunc faucibus a pellentesque sit',
+          author: 'Jon Doe',
+          created_at: '12/30/2022',
+          is_saved: false,
+        },
+        {
+          id: 4,
+          title: 'Video games',
+          body: 'tellus integer feugiat scelerisque varius morbi enim nunc faucibus a pellentesque sit',
+          author: 'Jon Doe',
+          created_at: '05/01/2023',
+          is_saved: false,
+        },
+      ],
+    }
+  },
+
+  // computed
+  getters: {
+    sorted() {
+      return this.posts.sort((a, b) => new Date(b.created_at) - new Date(a.created_at))
+    },
+  },
+
+  // methods
+  actions: {
+    addPost(post) {
+      this.posts.push({
+        id: this.posts.length + 1,
+        title: post.title,
+        body: post.body,
+        author: 'Neha Ahmed',
+        created_at: new Date().toLocaleDateString(),
+        is_saved: false,
+      })
+    },
+  },
+})
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,59 +1,17 @@
 <script setup>
+import MyWrapper from '@/components/MyWrapper.vue'
 import PostItem from '@/components/PostItem.vue'
+import { usePostsStore } from '@/stores/posts'
 
-const posts = [
-  {
-    id: 1,
-    title: 'This great book!',
-    body: 'tellus integer feugiat scelerisque varius morbi enim nunc faucibus a pellentesque sit',
-    author: 'Sarah Doe',
-    created_at: '11/06/2023',
-    is_saved: false,
-  },
-  {
-    id: 2,
-    title: 'Coding is fun!?',
-    body: 'tellus integer feugiat scelerisque varius morbi enim nunc faucibus a pellentesque sit',
-    author: 'Jon Doe',
-    created_at: '06/04/2023',
-    is_saved: false,
-  },
-  {
-    id: 3,
-    title: 'Vue js VS React',
-    body: 'tellus integer feugiat scelerisque varius morbi enim nunc faucibus a pellentesque sit',
-    author: 'Jon Doe',
-    created_at: '12/30/2022',
-    is_saved: false,
-  },
-  {
-    id: 4,
-    title: 'Video games',
-    body: 'tellus integer feugiat scelerisque varius morbi enim nunc faucibus a pellentesque sit',
-    author: 'Jon Doe',
-    created_at: '05/01/2023',
-    is_saved: false,
-  },
-]
+const postStore = usePostsStore()
 </script>
 
 <template>
-  <div v-for="post in posts" :key="post.id">
-    <div class="wrapper">
+  <div v-for="post in postStore.sorted" :key="post.id">
+    <MyWrapper>
       <PostItem :post="post" />
-    </div>
+    </MyWrapper>
   </div>
 </template>
 
-<style lang="scss" scoped>
-.wrapper {
-  margin: 2rem;
-  background: #fff;
-  box-shadow:
-    rgba(0, 0, 0, 0.1) 0px 10px 15px -3px,
-    rgba(0, 0, 0, 0.05) 0px 4px 6px -2px;
-  margin-bottom: 2rem;
-  border-radius: 1rem;
-  overflow: hidden;
-}
-</style>
+<style lang="scss" scoped></style>
